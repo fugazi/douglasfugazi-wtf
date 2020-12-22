173a9d808d35f35ed19f0aff08c25c5b88fb8d3b
Douglas Fugazi <itmteleco@gmail.com>
2020-12-21 20:49:06 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -52,16 +52,6 @@ export default class Header extends React.Component {
                   }
                 </div>
               </div>
-              // Google Analytics
-              <!-- Global site tag (gtag.js) - Google Analytics -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q33JRV9F9C"></script>
-                <script>
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-
-                  gtag('config', 'G-Q33JRV9F9C');
-                </script>
             </header>
         );
     }
