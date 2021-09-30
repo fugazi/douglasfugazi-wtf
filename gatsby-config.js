557eb236f23d346ebbd26b8378a8bcbb649d06a9
Douglas Fugazi <douglas.fugazi@gmail.com>
2021-09-29 20:10:50 -0500

    dios todopoderoso arregla este coso

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,28 +37,6 @@ module.exports = {
                 pageContextProperty: `menus`,
                 menus: require('./src/data/menus.json'),
             }
-        },
-		{
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                // The property ID; the tracking code won't be generated without it
-                trackingId: "G-Q33JRV9F9C",
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: true,
-            }
-        },
-		{
-            resolve: `gatsby-plugin-google-gtag`,
-            options: {
-                // You can add multiple tracking ids and a pageview event will be fired for all of them.
-				trackingIds: [
-				"G-Q33JRV9F9C", // Google Analytics 
-				],
-			pluginConfig: {
-			// Puts tracking script in the head instead of the body
-			head: true,
-				}
-			}
         }
     ]
 };
