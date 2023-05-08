570ff045ac176b383bf79278485613664b329fe2
Douglas Fugazi <itmteleco@gmail.com>
2023-05-08 14:30:42 -0500

    Update stackbit-build.sh
    updating douglas

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,7 +7,7 @@ set -v
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d76a6c6dfe24c0010bae567 
+    echo "This is fine"
 fi
 gatsby build
 ./inject-netlify-identity-widget.js public
